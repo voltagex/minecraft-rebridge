7dbddd5508b2cb3ebe589e81098de7768ba2e9f1
Adam Baxter <voltagex@voltagex.org>
2016-08-13 00:04:12 +1000

    Fix naming, prepare for removal of NanoHTTPD source

--- src/main/java/org/voltagex/rebridge/Consts.java
+++ src/main/java/org/voltagex/rebridge/Consts.java
@@ -3,6 +3,6 @@ package org.voltagex.rebridge;
 public class Consts
 {
     //todo: work out how to sync this to Gradle/mcmod.info versions
-    static final String MODID = "Rebridge";
-    static final String VERSION = "0.0.1";
+    static final String MODID = "rebridge";
+    static final String VERSION = "0.0.2";
 }
